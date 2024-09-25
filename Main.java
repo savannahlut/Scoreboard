65b385a3fcd3d7ce3c26b86d4832e84d525f0770
savannahlut <savannahlut@nycstudents.net>
2024-09-25 19:25:04 +0000

    Added recordPlay method

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
+        game.recordPlay(1);
         info = game.getScore();
         System.out.println(info);
     }
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,21 +1,23 @@
-public class Scoreboard
+public class Scoreboard 
 {
     private String teamOne;
     private String teamTwo;
     private int teamOneScore;
     private int teamTwoScore;
     private String activeTeam;
-}
 
-public Scoreboard (String one, String two)
-{
-    teamOne = one;
-    teamTwo = two;
-    activeTeam = teamOne;
-}
+    public Scoreboard(String one, String two) {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
 
-public String getScore()
-{
-    return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    public String getScore() {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
 
+    public void recordPlay(int score)
+    {
+        
+    }
 }
