a32c886f6df080674943e799c2d8e6c351243f6f
savannahlut <savannahlut@nycstudents.net>
2024-09-25 18:50:59 +0000

    Added Strings

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,21 @@
-public class Scoreboard{}
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+}
+
+public Scoreboard (String one, String two)
+{
+    teamOne = one;
+    teamTwo = two;
+    activeTeam = teamOne;
+}
+
+public String getScore()
+{
+    return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+
+}
