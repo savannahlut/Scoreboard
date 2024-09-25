d5a4e0222ae55a66a6cc24b2feea31436d80adc8
savannahlut <savannahlut@nycstudents.net>
2024-09-25 13:32:47 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+    }
+}
